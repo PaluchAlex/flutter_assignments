95c496e2d55e50accc425326bd082375d0a5304e
DESKTOP-EFDVTUD\Alex <alexpaluch43@gmail.com>
2023-12-01 16:04:25 +0200

    reset for draw

--- lib/src/homework4/tiktaktoe.dart
+++ lib/src/homework4/tiktaktoe.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const TikTakToe());
@@ -53,6 +54,26 @@ class _HomePageState extends State<HomePage> {
     return generatedSquares;
   }
 
+  bool checkForWin(){
+    return false;
+  }
+
+  String checkStateOfGame(){
+    if(checkForWin()){
+      return 'win';
+    }
+    //check diagonals
+    //check lines
+    //check columns
+
+    //check draw
+    if(checkForDraw()){
+      return 'draw';
+    }
+
+    return 'ongoing';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,26 +81,62 @@ class _HomePageState extends State<HomePage> {
         title: const Text('TikTakToe'),
         centerTitle: true,
       ),
-      body: GridView.count(
-        crossAxisCount: 3,
-        mainAxisSpacing: 24,
-        crossAxisSpacing: 24,
-        children: <Widget>[
-          for (int x = 0; x < 3; x++)
-            for (int y = 0; y < 3; y++)
-              BoardSquare(
-                  isX: isX,
-                  board: board,
-                  boardX: x,
-                  boardY: y,
-                  onPressed: () {
-                    setState(() {
-                      isX = !isX;
-                    });
-                  })
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.count(
+          crossAxisCount: 3,
+          children: <Widget>[
+            for (int x = 0; x < 3; x++)
+              for (int y = 0; y < 3; y++)
+                BoardSquare(
+                    isX: isX,
+                    board: board,
+                    boardX: x,
+                    boardY: y,
+                    onPressed: () {
+                      if(checkStateOfGame() != 'ongoing'){
+                        setState(() {
+                          if (kDebugMode) {
+                            print('${checkStateOfGame()}! board resetting...');
+                          }
+                          resetBoard();
+                        });
+
+                      } else {
+                        setState(() {
+                          isX = !isX;
+                          //TO:DO check for win
+
+                        });
+                      }
+                    })
+          ],
+        ),
       ),
     );
+  }
+
+  bool checkForDraw() {
+    bool draw = true;
+    List<bool?> line;
+    for(line in board) {
+      if(line.contains(null)){
+        /// if a square is empty draw = false
+        draw = false;
+      }
+    }
+    return draw;
+  }
+
+  void resetBoard() {
+    /// empty board
+    board = <List<bool?>>[
+      <bool?>[null, null, null],
+      <bool?>[null, null, null],
+      <bool?>[null, null, null]
+    ];
+    /// X starts
+    isX = true;
   }
 }
 
@@ -87,9 +144,13 @@ class BoardSquare extends StatefulWidget {
   const BoardSquare(
       {super.key,
       required this.isX,
+        /// this variable indicates if its the turn for X or O to play
       required this.board,
+        /// the local memory board
       required this.boardX,
+        /// the X position of the current widget on the board
       required this.boardY,
+        /// the Y position of the current widget on the board
       required this.onPressed});
 
   final List<List<bool?>> board;
@@ -103,9 +164,8 @@ class BoardSquare extends StatefulWidget {
 }
 
 class _BoardSquareState extends State<BoardSquare> {
-  bool isClear = true;
 
-  void updateBoard(bool isX, List<List<bool?>> board, int x, int y) {
+  void updateMemoryBoard(bool isX, List<List<bool?>> board, int x, int y) {
     board[x][y] = isX;
   }
 
@@ -113,37 +173,38 @@ class _BoardSquareState extends State<BoardSquare> {
   Widget build(BuildContext context) {
     return Card(
       child: InkWell(
-        onTap: () {
-          setState(() {
-            if (isClear) {
-              updateBoard(widget.isX, widget.board, widget.boardX, widget.boardY);
-            }
-            isClear = false;
-          });
-
-          if (kDebugMode) {
-            print(widget.board);
-          }
-          widget.onPressed();
-        },
+        onTap: widget.board[widget.boardX][widget.boardY] == null ? (){
+            setState(() {
+              updateMemoryBoard(widget.isX, widget.board, widget.boardX, widget.boardY);
+            });
+            widget.onPressed(); //register press outside of widget
+          } : null,
+          /// if square is not empty does nothing onTap
         child: Center(
-          child: isClear ? Container() : followBoard(widget.board),
+          child: updateUIBoard(widget.board),
+          /// update UI board if square not clear
         ),
       ),
     );
   }
 
-  Widget followBoard(List<List<bool?>> board) {
-    if (board[widget.boardX][widget.boardY] ?? true) {
-      return const Text(
-        'X',
-        style: TextStyle(fontSize: 100),
-      );
-    } else {
-      return const Text(
-        'O',
-        style: TextStyle(fontSize: 100),
-      );
+  Widget updateUIBoard(List<List<bool?>> board) {
+    /// true = X, false = O, null = empty
+    if(board[widget.boardX][widget.boardY] != null){
+
+      if (board[widget.boardX][widget.boardY] ?? true) {
+        return Text(
+          'X',
+          style: GoogleFonts.handlee(textStyle: const TextStyle(fontSize: 100)),
+        );
+      } else {
+        return Text(
+          'O',
+          style: GoogleFonts.handlee(textStyle: const TextStyle(fontSize: 100)),
+        );
+      }
     }
+    return Container();
+
   }
 }
