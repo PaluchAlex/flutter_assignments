6655526e9a8ec68b7b5133f9828ea148794f60e2
DESKTOP-EFDVTUD\Alex <alexpaluch43@gmail.com>
2023-12-06 15:06:44 +0200

    basic layout

--- lib/src/homework5/gallery.dart
+++ lib/src/homework5/gallery.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+import 'dart:core';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import '../../config.dart';
 
 void main() {
-  runApp(Gallery());
+  runApp(const Gallery());
 }
 
 class Gallery extends StatelessWidget {
@@ -12,7 +16,7 @@ class Gallery extends StatelessWidget {
     return MaterialApp(
       title: 'Gallery',
       theme: ThemeData(useMaterial3: false),
-      home: Home(),
+      home: const Home(),
     );
   }
 }
@@ -25,6 +29,36 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final List<Photo> items = <Photo>[];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    loadItems();
+  }
+
+  Future<void> loadItems() async {
+    const String accessKey = AppConfig.apiKey;
+
+    final Client client = Client();
+    final Response response = await client.get(
+        Uri.parse('https://api'
+            '.unsplash.com/photos/'),
+        headers: <String, String>{'Authorization': 'Client-ID $accessKey'});
+    if (response.statusCode == 200) {
+      /// cast decoded as Map<String, dynamic>
+      final List<dynamic> decoded = jsonDecode(response.body) as List<dynamic>;
+
+      /// add to items
+      for (final dynamic item in decoded) {
+        items.add(Photo(item as Map<String, dynamic>));
+      }
+
+      setState(() => isLoading = false);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +66,43 @@ class _HomeState extends State<Home> {
         title: const Text('Gallery'),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Builder(
+        builder: (BuildContext context) {
+          if (isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return ListView.builder(
+            padding: const EdgeInsets.all(8.0),
+            itemCount: items.length,
+            itemBuilder: (BuildContext context, int index) {
+              final Photo photo = items[index];
+
+              return Column(
+                children: <Widget>[
+                  Image.network(photo.urls['raw'] as String),
+                  ListTile(
+                    title: Text('Likes: ${photo.likes}'),
+                    subtitle: Text('Description: ${photo.altDescription}'),
+                  )
+                ],
+              );
+            },
+          );
+        },
+      ),
     );
   }
+}
+
+class Photo {
+  Photo(Map<String, dynamic> json)
+      : urls = json['urls'] as Map<String, dynamic>,
+        altDescription = json['alt_description'] as String,
+        likes = json['likes'] as int,
+        user = json['user'] as Map<String, dynamic>;
+
+  final Map<String, dynamic> urls;
+  final String altDescription;
+  final int likes;
+  final Map<String, dynamic> user;
 }
